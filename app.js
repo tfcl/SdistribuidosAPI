eb4229b7dec228582f7b5d6fd2813c0817b40219
Tiago Filipe da Costa Lourenço <tiago9l@hotmail.com>
2020-10-23 20:11:14 +0200

    Added a ui

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ const express=require('express');
 const app = express();
 const mongoose =require('mongoose');
 const bodyParser=require('body-parser');
-
+const favicon = require('serve-favicon');
+const path = require('path');
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+app.set("view engine", "ejs");
 require('dotenv/config');
 
 //import resources
 const booksRoute=require('./routes/books');
 app.use(bodyParser.json());
-
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 
 //middlewares
 app.use('/books',booksRoute);
-
+app.get('/favico.ico', (req, res) => {
+    res.sendStatus(404);
+});
 //Routes
 app.get('/',(req,res)=>{
-    res.send('we are on');
+    res.render('we are on');
 
 });
 //Connect ro db
--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,30 @@
+const { urlencoded } = require('body-parser');
 const { Router } = require('express');
 const express =require('express');
 const router = express.Router();
 const Book =require('../models/Book');
+const app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+const methodOverride = require('method-override');
 //gets all the books
-router.get('', async(req,res)=>{
+router.get('/', async(req,res)=>{
     try{
+        console.log(req.query.id)
+        if (req.query.id !== undefined){
+            if(req.query.id.length > 0) {
+                
+                res.redirect("/books/"+req.query.id)
+                console.log("entrou");
+
+            }
+    }
+        console.log("home");
         const books=await Book.find();
-        res.json(books)
+        res.render('Index',{books:books})
+        //res.json(books)
+
 
     }catch(err){
         res.json({message:err});
@@ -19,7 +36,8 @@ router.get('', async(req,res)=>{
 
 
 //submits a book
-router.post('/',async (req,res)=>{
+router.post('/insert',urlencodedParser,async (req,res)=>{
+    console.log(req.body);
     const book=new Book({
         title:req.body.title,
         isAvailable:req.body.isAvailable,
@@ -29,19 +47,38 @@ router.post('/',async (req,res)=>{
     });
     try{
     const savedBook= await book.save();
-    res.json(savedBook);
+    res.redirect("/books")
     }catch(err){
         res.json({message:err});
     }
 });
+router.get('/edit',async(req,res)=>{
+    try{
+        console.log(req.query.id);
 
+        
+        res.render('edit',{id:req.query.id});
+    }catch(err){
+        res.json({message:err});
+
+    }
+});
 
 //get specicfic book
+router.get('/insert',async(req,res)=>{
+    try{
+        res.render('insert');
+    }catch(err){
+        res.json({message:err});
 
+    }
+});
 router.get('/:bookId', async(req,res)=>{ 
     try{
+
+    
     const book = await Book.findById(req.params.bookId);
-    res.json(book)
+    res.render('Index1',{book:book})
     }catch(err){
         res.json({message:err});
 
@@ -49,50 +86,50 @@ router.get('/:bookId', async(req,res)=>{
 
 });
 
-
+//edit view
 //Delete Post
 router.delete('/:bookId', async(req,res)=>{
     try{
-    const removedBook= await Book.remove({_id: req.params.bookId});
-    res.json(removedBook);
+        const removedBook= await Book.remove({_id: req.params.bookId});
+        res.redirect("/books")
     }catch(err){
         res.json({message:err});
-
-
+        
+        
     }
-
+    
 });
 
 //Update a book
-router.patch('/:bookId', async(req,res)=>{
+router.patch('/:bookId',urlencodedParser, async(req,res)=>{
     try{
-    const updatedBook= await Book.updateOne(
-        {_id: req.params.bookId}, 
-        {$set: {title: req.body.title}}
+        const updatedBook= await Book.updateOne(
+            {_id: req.params.bookId}, 
+            {$set: {title: req.body.title}}
+            
+            );
+            res.redirect("/books")
+        }catch(err){
+            res.json({message:err});
+            
+            
+        }
         
-    );
-    res.json(updatedBook);
-    }catch(err){
-        res.json({message:err});
+    });
+    
+    //get avalable books
+    router.get('/available/:param', async(req,res)=>{
+        try{
+            const books= await Book.find({isAvailable:req.params.param});
+            res.render('Index',{books:books})
+        }catch(err){
+            res.json({message:err});
+            
+            
+        }
+        
+    });
+    
 
-
-    }
-
-});
-
-//get avalable books
-router.get('/available/:param', async(req,res)=>{
-    try{
-    const books= await Book.find({isAvailable:req.params.param});
-    console.log(books);       
-
-    res.json(books);
-    }catch(err){
-        res.json({message:err});
-
-
-    }
-
-});
-
-module.exports = router;
+    
+    module.exports = router;
